6ad926f8de2c108233c914a794fa08e2c830ce89
henry-the-vietnamese <henryfromvietnam@gmail.com>
2021-10-06 02:33:50 +1030

    add a secure version

--- main.py
+++ main.py
@@ -32,6 +32,13 @@ MAC = new_mac.UserChoice()
 print(f'[+] Changing the MAC address for {interface} to {MAC}.')
 
 # The process of changing the current MAC address.
-subprocess.call(f'ifconfig {interface} down', shell=True)
-subprocess.call(f'ifconfig eth0 hw ether {MAC}', shell=True)
-subprocess.call(f'ifconfig {interface} up', shell=True)
+
+## Less secure verison 
+#subprocess.call(f'ifconfig {interface} down', shell=True)
+#subprocess.call(f'ifconfig eth0 hw ether {MAC}', shell=True)
+#subprocess.call(f'ifconfig {interface} up', shell=True)
+
+## More secure version
+subprocess.call(['ifconfig', interface, 'down'])
+subprocess.call(['ifconfig', interface, 'hw', 'ether',  MAC])
+subprocess.call(['ifconfig', interface, 'up'])
